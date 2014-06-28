c5214b50e6c1ef5d26794d98ed1a7dd3c2530507
JoeyKL <joey@kellison-linn.org>
2014-06-28 12:34:19 -0700

    Create HumanController class w/ two joysticks

--- MistahKrabbs/HumanController.h
+++ MistahKrabbs/HumanController.h
@@ -1,1 +1,18 @@
-// there will be stuff
+#ifndef HUMANCONTROLLER_H
+#define HUMANCONTROLLER_H
+#include "Robot.h"
+
+class HumanController {
+public:
+	void update();
+	HumanController(Robot* robot)
+	
+private:
+	Robot* robot;
+	Joystick moveStick;
+	Joystick turnStick;
+	
+	double getAbsMoveStick();
+	double getAbsTurnStick();
+}
+#endif
