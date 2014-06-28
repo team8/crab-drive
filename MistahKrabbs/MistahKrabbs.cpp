3206691729e96ab062c886ed289c741b9bb6ad49
neelayjunnarkar <neelay.junnarkar@gmail.com>
2014-06-28 16:56:55 -0700

    MistahKrabbs.cpp stuffness

--- MistahKrabbs/MistahKrabbs.cpp
+++ MistahKrabbs/MistahKrabbs.cpp
@@ -1,15 +1,65 @@
+#include "Constants.h"
 #include "Robot.h"
 #include "HumanController.h"
+#include <iostream>
 
 namespace Krabbs {
 
 	class MistahKrabbs : public IterativeRobot {
 	private:
-	
+		Robot robot;
+		HumanControllor humanCtrlr;
 	public:
 		MistahKrabbs();
 		virtual ~MistahKrabbs();
 		
+		void RobotInit();
+		void DisabledInit();
+		void DisabledPeriodic();
+		void TeleopInit();
+		void TeleopPeriodic();
+		void TeleopDisabled();
 	};
 	
 }
+
+using namespace Krabbs;
+
+MistahKrabbs::MistahKrabbs():
+robot(),
+humanCtrlr(&robot)
+{
+	std::cout << "MistahKrabbs constructor" << std::endl;
+}
+
+
+void MistahKrabbs::RobotInit() {
+	std::cout << "robot init" << std::endl;
+}
+
+void MistahKrabbs::DisabledInit() {
+	robot.disable();
+	std::cout << "robot disable" << std::endl;
+}
+
+void MistahKrabbs::DisabledPeriodic() {
+	robot.disable();
+	robot.update();
+}
+
+void MistahKrabbs::TeleopInit() {
+	std::cout << "Teleop init" << std::endl;
+	robot.init();
+}
+
+void MistahKrabbs::TeleopPeriodic() {
+	humanCtrlr.update();
+	robot.update();
+}
+
+void MistahKrabbs::TeleopDisabled() {
+	robot.disable();
+	robot.update();
+}
+
+START_ROBOT_CLASS(Krabbs::MistahKrabbs);
