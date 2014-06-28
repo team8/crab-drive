2b251034672b1224843a5bf9a284be46b2c4f14d
James Ngo <ducngocngo2@gmail.com>
2014-06-28 13:16:03 -0700

    made enums and commands

--- MistahKrabbs/Robot.h
+++ MistahKrabbs/Robot.h
@@ -10,6 +10,18 @@ class Robot {
 	public:
 		Robot();
 		void update();
+		typedef enum DriveCommands {
+			ANGLE_ALL_WHEELS,
+			MOVE_ALL_WHEELS,
+			ANGLE_FRONT_WHEELS,
+			ANGLE_BACK_WHEELS-
+		}
+		typedef union DriveArgs {
+			double allWheelAngle,
+			double allWheelSpeed,
+			double frontWheelAngle,
+			double backWheelAngle
+		}
 
 }
 #endif
