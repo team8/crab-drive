4c74457d535b3c6a191e5a6310849ed852380b69
James Ngo <ducngocngo2@gmail.com>
2014-06-28 16:58:11 -0700

    added comments and constants to be defined later

--- MistahKrabbs/Constants.h
+++ MistahKrabbs/Constants.h
@@ -1,3 +1,24 @@
+//stick ports
 #define PORT_SPEED 1
 #define PORT_TURN 2
 #define PORT_OPERATOR 3
+
+//wheel ports
+#define LEFT_FRONT_SPEED_VIC 0
+#define LEFT_FRONT_ANGLE_VIC 0
+#define LEFT_FRONT_ANGLE_ENC 0
+
+#define RIGHT_FRONT_SPEED_VIC 0
+#define RIGHT_FRONT_ANGLE_VIC 0
+#define RIGHT_FRONT_ANGLE_ENC 0
+
+#define LEFT_BACK_SPEED_VIC 0
+#define LEFT_BACK_ANGLE_VIC 0
+#define LEFT_BACK_ANGLE_ENC 0
+
+#define RIGHT_BACK_SPEED_VIC 0
+#define RIGHT_BACK_ANGLE_VIC 0
+#define RIGHT_BACK_ANGLE_ENC 0
+
+//speed wheelunits change angle
+#define ANGLE_SPEED 0.5
