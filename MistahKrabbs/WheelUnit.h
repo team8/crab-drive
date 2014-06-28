c6f4d10e3d69f3a03869f9366505d51f7243ced6
James Ngo <ducngocngo2@gmail.com>
2014-06-28 16:53:43 -0700

    took out auto added update

--- MistahKrabbs/WheelUnit.h
+++ MistahKrabbs/WheelUnit.h
@@ -2,11 +2,28 @@
 #define WHEEL_UNIT_H
 #include <WPILib.h>
 
+namespace Krabbs {
+
 class WheelUnit {
   private:
-  
+    Victor speedVic;
+    Victor angleVic;
+    Encoder angleEnc;
+    double targetAngle;
+    double targetSpeed;
+    
   public:
+    WheelUnit(int speedVicPort, int angleVicPort, int angleEncPort);
+    //set speed of speedVic
+    setSpeed(double speed);
+    //set angle of wheel
+    setAngle(double angle);
+    //get angle
+    double getAngle();
+    
+    update();
 
 };
 
+}
 #endif
