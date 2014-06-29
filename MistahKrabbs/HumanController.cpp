2acdaaceb9709ad29900ff8bed84aaef22634d7c
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2014-06-29 11:40:42 -0700

    semi-colons tend to be good

--- MistahKrabbs/HumanController.cpp
+++ MistahKrabbs/HumanController.cpp
@@ -4,7 +4,7 @@
 
 using namespace Krabbs;
 
-HumanController::HumanController(Robot* robotPointer):
+HumanController::HumanController(Robot *robotPointer):
 	moveStick(PORT_MOVE_STICK),
 	turnStick(PORT_TURN_STICK)
 {
@@ -12,20 +12,20 @@ HumanController::HumanController(Robot* robotPointer):
 }
 
 void HumanController::update() {
-	robot->drivetrainCommand(Robot::DrivetrainCommand.ANGLE_FRONT_WHEELS, getAngMoveStick() + getAbsTurnStick())
-	robot->drivetrainCommand(Robot::DrivetrainCommand.ANGLE_BACK_WHEELS , getAngMoveStick() - getAbsTurnStick())
+	robot->drivetrainCommand(Robot::DrivetrainCommand.ANGLE_FRONT_WHEELS, getAngMoveStick() + getAbsTurnStick());
+	robot->drivetrainCommand(Robot::DrivetrainCommand.ANGLE_BACK_WHEELS , getAngMoveStick() - getAbsTurnStick());
 	
-	robot->drivetrainCommand(Robot::DrivetrainCommand.MOVE_ALL_WHEELS, getMagMoveStick())
+	robot->drivetrainCommand(Robot::DrivetrainCommand.MOVE_ALL_WHEELS, getMagMoveStick());
 }
 
 double HumanController::getAngMoveStick() {
-	return atan2(moveStick.getY(), moveStick.getX())
+	return atan2(moveStick.getY(), moveStick.getX());
 }
 
 double HumanController::getMagMoveStick() {
-	return sqrt(pow(moveStick.getX(), 2.0) + pow(moveStick.getY(), 2.0))/sqrt(2.0)
+	return sqrt(pow(moveStick.getX(), 2.0) + pow(moveStick.getY(), 2.0))/sqrt(2.0);
 }
 
 double HumanController::getAbsTurnStick() {
-	return abs(turnStick().getX())
+	return abs(turnStick().getX());
 }
